539866fca31129ba51dbffe8865bde9914cffa2e
NguyenNVPK02647 <nguyennvpk02647@fpt.edu.vn>
2024-03-15 10:47:02 +0700

    fix

--- src/_auth/forms/SignupForm.tsx
+++ src/_auth/forms/SignupForm.tsx
@@ -3,7 +3,6 @@ import { Button } from "@/components/ui/button"
 import { Link } from "react-router-dom"
 import { zodResolver } from "@hookform/resolvers/zod"
 import { useForm } from "react-hook-form"
-
 import { Form, FormControl, FormField, FormItem, FormLabel, FormMessage, } from "@/components/ui/form"
 import { Input } from "@/components/ui/input"
 import { SignupValidation } from "@/lib/validation"
